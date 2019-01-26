7686d91145241920b416b84b8a36f6166e46329d
dominik <dominikpeter@users.noreply.github.com>
2019-01-27 00:29:06 +0100

    Update BootstrapRegression.py

--- BootstrapRegression.py
+++ BootstrapRegression.py
@@ -15,19 +15,20 @@ class BootstrapRegression(LinearRegression):
             np.arange(0, len(X)), size=len(X))
         return X[idx], y[idx]
     
-    def bootstrap(self, X, y, n=1000, **kwargs):
+    def bootstrap(self, X, y, n=1000, seed=2323, **kwargs):
+        np.random.seed(seed)
         self.boot_coef = np.zeros(shape=n)
         self.boot_intercept = np.zeros(shape=n)
         self.boot_score = np.zeros(shape=n)
         for i in range(n):
             X_r, y_r = self._get_random_sample(X, y)
-            super().fit(X_r, y_r, **kwargs)
+            self.fit(X_r, y_r, **kwargs)
             self.boot_coef[i] = self.coef_
             self.boot_intercept[i] = self.intercept_
             self.boot_score[i] = self.score(X_r, y_r)
         return self
     
-    def get_conf(self, q=(2.5, 97.5)):
+    def get_confint(self, q=(2.5, 97.5)):
         intercept = np.percentile(
             self.boot_intercept, q=q)
         coef = np.percentile(
@@ -36,12 +37,10 @@ class BootstrapRegression(LinearRegression):
             self.boot_score, q=q)
         return intercept, coef, score
     
-    def get_mean(self):
+    def get_mean_param(self):
         intercept = self.boot_intercept.mean()
         coef = self.boot_coef.mean()
         score = self.boot_score.mean()
         return intercept, coef, score
-
-
 
 
